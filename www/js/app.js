34ea9b9750630cf3148c21f9aff14037af79fead
DuskyShelf <dupsdale@gmail.com>
2015-08-14 16:09:11 +0100

    added pigeon placeholder metric

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,17 @@ angular.module('starter', ['ionic', 'ngCordova'])
 .controller('imageController', function($scope, $cordovaCamera, $cordovaFile) {
   $scope.images = JSON.parse(window.localStorage.images || '[]');
 
+  $scope.placeheldgallery = [];
+
+  for(var i=0; i < 12; i++) {
+    if($scope.images[i]) {
+      $scope.placeheldgallery[i] = $scope.images[i];
+    }
+    else {
+      $scope.placeheldgallery[i] = {url: "img/pigeonplaceholder.gif"};
+    }
+  }
+
   $scope.addImage = function(photoIndex) {
 
     var options = {
@@ -16,6 +27,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
     $cordovaCamera.getPicture(options).then(function(imageData) {
 
     $scope.images[photoIndex] = {url: imageData};
+    $scope.placeheldgallery[photoIndex] = {url: imageData};
 
     window.localStorage.images = JSON.stringify($scope.images);
 
